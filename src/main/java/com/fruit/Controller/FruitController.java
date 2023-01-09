65c4bd1a28f809064dbd11ed761fc33fe6d1ef85
MAAZAOUI Samir (Externe) <samir.maazaoui-ext@elis.com>
2023-01-09 17:40:24 +0100

    add log score

--- src/main/java/com/fruit/Controller/FruitController.java
+++ src/main/java/com/fruit/Controller/FruitController.java
@@ -3,12 +3,14 @@ package com.fruit.Controller;
 import com.fruit.service.FruitServiceImpl;
 import com.fruit.service.Slot;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
+@Slf4j
 public class FruitController {
 
     private final FruitServiceImpl fruitService;
@@ -17,6 +19,7 @@ public class FruitController {
     public String getFruitsshowUserList(Model model) {
         Slot slot = this.fruitService.calculateScore();
         model.addAttribute("slot", slot);
+        log.info("le score est : "+slot.getScore());
         return "index.html";
     }
 
